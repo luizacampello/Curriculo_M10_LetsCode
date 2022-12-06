121fd2085a3d28256b59f225a442e7e077fdf0cf
Matheus Amaro <mat.amaro427@gmail.com>
2022-12-06 18:41:21 -0300

    add home-container component

--- projetoCurriculo/src/app/app.module.ts
+++ projetoCurriculo/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeContainerComponent } from './components/home-container/home-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeContainerComponent
   ],
   imports: [
     BrowserModule
