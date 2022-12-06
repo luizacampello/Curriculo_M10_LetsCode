d34b90a31e98b1e3da78adeeb11ee73412cf73e0
carvalhosmari <mscarvall@gmail.com>
2022-12-06 18:38:01 -0300

    add about-me-container

--- projetoCurriculo/src/app/app.module.ts
+++ projetoCurriculo/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactFormContainerComponent } from './components/contact-form-container/contact-form-container.component';
+import { AboutMeContainerComponent } from './components/about-me-container/about-me-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormContainerComponent
+    ContactFormContainerComponent,
+    AboutMeContainerComponent
   ],
   imports: [
     BrowserModule
