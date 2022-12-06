cca870adf143111a2e042c35bc6aced06dd26ef1
Luiza Campello <lumcampello@gmail.com>
2022-12-06 18:26:44 -0300

    add app-contact-form-container to app component html

--- projetoCurriculo/src/app/app.module.ts
+++ projetoCurriculo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContactFormContainerComponent } from './contact-form-container/contact-form-container.component';
+import { ContactFormContainerComponent } from './components/contact-form-container/contact-form-container.component';
 
 @NgModule({
   declarations: [
