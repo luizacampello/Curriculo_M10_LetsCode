4d1ffd601ceffbb827480df01f3654d6048bc463
carvalhosmari <mscarvall@gmail.com>
2022-12-14 12:34:09 -0300

    add Typewriter effect

--- projetoCurriculo/src/app/components/home-inner/home-inner.component.ts
+++ projetoCurriculo/src/app/components/home-inner/home-inner.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-inner',
   templateUrl: './home-inner.component.html',
   styleUrls: ['./home-inner.component.css']
 })
-export class HomeInnerComponent {
+export class HomeInnerComponent implements OnInit {
+
   nome = "Luiza Motta Campello";
 
+  ngOnInit(): void {
+
+    const Typewriter = require('t-writer.js');
+
+    const target = document.querySelector('.target');
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 80,
+      deleteSpeed: 80,
+    });
+
+    writer
+      .changeTypeClass('teste')
+      .type('geóloga')
+      .rest(500)
+      .remove(7)
+      .type('programadora')
+      .rest(500)
+      .remove(12)
+      .type('louca por gatinhos')
+      .rest(500)
+      .remove(18)
+      .start()
+
+  }
 }
