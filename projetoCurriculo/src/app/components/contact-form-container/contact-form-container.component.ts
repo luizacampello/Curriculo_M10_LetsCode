2caf1d5847045187503935501c3b63adf25f0aa1
Matheus Amaro <mat.amaro427@gmail.com>
2022-12-14 18:23:18 -0300

    adding input validation into contact form

--- projetoCurriculo/src/app/components/contact-form-container/contact-form-container.component.ts
+++ projetoCurriculo/src/app/components/contact-form-container/contact-form-container.component.ts
@@ -1,6 +1,6 @@
 import { ContactFormData } from './../../models/contact-form-data.model';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form-container',
@@ -17,16 +17,22 @@ export class ContactFormContainerComponent {
 
   private buildForm(): void {
     this.form = new FormGroup({
-      name: new FormControl(),
-      email: new FormControl(),
-      message: new FormControl(),
+      name: new FormControl("",Validators.required),
+      email: new FormControl("",Validators.required),
+      message: new FormControl("",Validators.required),
     });
   }
 
   public submitForm(): void {
     this.formData = this.form.getRawValue();
-    
-    console.log(this.formData.email);
+
+    console.log(this.formData);
   }
+
+  get name() { return this.form.get('name')!; };
+
+  get email() { return this.form.get('email')!; };
+
+  get message() { return this.form.get('message')!; };
 
 }
