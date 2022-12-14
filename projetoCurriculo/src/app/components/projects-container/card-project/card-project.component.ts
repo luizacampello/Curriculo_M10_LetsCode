174a3e3a1b249adde8353e184860e1b90e1038a4
Marco Antonio <cootoonhoo@gmail.com>
2022-12-14 16:32:31 -0300

    git s

--- projetoCurriculo/src/app/components/projects-container/card-project/card-project.component.ts
+++ projetoCurriculo/src/app/components/projects-container/card-project/card-project.component.ts
@@ -1,3 +1,5 @@
+import { DOCUMENT } from '@angular/common';
+import { HomeInnerComponent } from './../../home-inner/home-inner.component';
 import { Component, Input } from '@angular/core';
 import { cardContent } from 'src/app/models/card-content.model';
 
@@ -8,4 +10,19 @@ import { cardContent } from 'src/app/models/card-content.model';
 })
 export class CardProjectComponent {
 @Input() cardData!:cardContent;
+imageHasLoaded: boolean = false;
+
+ngAfterViewInit(){
+  if(!this.imageHasLoaded){
+    this.reDoFrontCard();
+  }
+}
+
+hasLoaded(){
+ this.imageHasLoaded = true;
+}
+
+reDoFrontCard(){
+  this.innerHtml += null;
+}
 }
