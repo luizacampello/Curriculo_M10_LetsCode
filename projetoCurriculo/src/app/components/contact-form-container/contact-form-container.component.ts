8219ec39245a2becbdcb5f4a8c94fe0d9b496267
Matheus Amaro <mat.amaro427@gmail.com>
2022-12-16 14:31:56 -0300

    fix  responsiveness in the contact form

--- projetoCurriculo/src/app/components/contact-form-container/contact-form-container.component.ts
+++ projetoCurriculo/src/app/components/contact-form-container/contact-form-container.component.ts
@@ -18,7 +18,7 @@ export class ContactFormContainerComponent {
   private buildForm(): void {
     this.form = new FormGroup({
       name: new FormControl("",Validators.required),
-      email: new FormControl("",[Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      email: new FormControl("",[Validators.required, Validators.email, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")]),
       message: new FormControl("",[Validators.required, Validators.minLength(3)]),
     });
   }
